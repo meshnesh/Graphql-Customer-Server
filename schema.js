3abd2d85a8f986948bfe45c52299fc8d09b17295
Antony Ng'ang'a <antony_mn@Antonys-MBP.mshome.net>
2017-09-05 07:18:50 +0300

    used axios to fetch data

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -39,17 +40,23 @@ const RootQuery = new GraphQLObjectType({
                 id: {type: GraphQLString}
             },
             resolve(parentValue, args){
+                /*
                 for(let i = 0;i< customers.length;i++){
                     if(customers[i].id == args.id){
                         return customers[i];
                     }
                 }
+                */
+
+                return axios.get('http://localhost:3000/customers/' + args.id)
+                    .then(res => res.data);
             }
         },
         customers:{
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args){
-                return customers;
+                return axios.get('http://localhost:3000/customers/')
+                .then(res => res.data);
             }
         }
     }
